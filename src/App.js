8c9e6c1f2743516d27f702014bf93e9fabc1f7c0
faisalmirji123 <faisalmirji@gmail.com>
2021-11-24 00:25:28 +0530

    useState used

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./style.css";
+import { useState } from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
+    <div style={{ textAlign: "center", marginTop: 100 }}>
+      <h1>Hello World</h1>
       <p>Start editing to see some magic happen :)</p>
+      {/* function Example() { */}
+      {/* // Declare a new state variable, which we'll call "count" */}
+      <div style={{ textAlign: "center" }}>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
+      </div>
     </div>
   );
 }
